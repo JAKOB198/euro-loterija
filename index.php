104df4a86971a5c34fc04e23e20ce29795488874
Jakob544 <jakobmehmc1@gmail.com>
2025-05-23 10:25:33 +0200

    zacetek

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="index.css" style="stylesheet">
+    <title>loterija</title>
+</head>
+<body>
+    <div class="header">
+
+    <p>jakobeeeeeeeeeeeeeeee</p>
+    </div>
+</body>
+</html>
